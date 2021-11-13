4cb6c63ece69a362532f73ddce70fae55a2be728
h3nnn4n <uber.renan@gmail.com>
2021-11-13 11:43:04 -0300

    Move Actor to a separate file

--- colosseum/games/food_catcher/game.py
+++ colosseum/games/food_catcher/game.py
@@ -1,69 +1,12 @@
 import logging
-import random
-import string
 from collections import defaultdict
 from random import shuffle, uniform
 
 import numpy as np
 
-from colosseum.utils import object_distance
+from colosseum.utils import object_distance, random_id
 
-
-# TODO: Move this to utils
-def random_id():
-    return "".join(
-        random.choices(
-            string.ascii_lowercase + string.ascii_uppercase + string.digits, k=6
-        )
-    )
-
-
-class Actor:
-    def __init__(self):
-        self.position = None
-        self.id = random_id()
-        self.owner_id = None
-        self.speed = 0.5
-        self.food = 0
-
-    def set_owner(self, owner_id):
-        self.owner_id = owner_id
-        return self
-
-    def set_position(self, position):
-        self.position = position
-        return self
-
-    def add_food(self, food):
-        self.food += food
-
-    def move(self, target):
-        target = np.array(target)
-        actor_position = np.array(self.position)
-        move_direction = target - actor_position
-        distance_to_target = np.linalg.norm(move_direction)
-        distance_to_move = min(self.speed, distance_to_target)
-
-        if distance_to_move > 1e-4:
-            actor_position_new = actor_position + (
-                move_direction / distance_to_target * distance_to_move
-            )
-            self.position = tuple(actor_position_new.tolist())
-            logging.info(
-                f"actor {self.id} moved from {actor_position} to {actor_position_new} with target {target} speed {distance_to_move}"
-            )
-            return
-
-        logging.info(f"actor {self.id} {actor_position=} is already at {target=}")
-
-    @property
-    def state(self):
-        return {
-            "position": self.position,
-            "id": self.id,
-            "owner_id": self.owner_id,
-            "food": self.food,
-        }
+from .actor import Actor
 
 
 class Food:
