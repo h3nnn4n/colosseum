c236231a969621cefec43146bf2afbca25fe80de
h3nnn4n <uber.renan@gmail.com>
2021-11-13 23:16:27 -0300

    Implement nearest first using sdk

--- agents/food_catcher_exterminator.py
+++ agents/food_catcher_exterminator.py
@@ -5,12 +5,15 @@ import logging
 from sdks.python.colosseum_sdk import Agent
 
 
-AGENT_NAME = "Exterminator"
+AGENT_NAME = "exterminator"
 
 
 class Exterminator(Agent):
     def __init__(self):
         super(Exterminator, self).__init__(agent_name=AGENT_NAME)
+
+    def post_state_update(self):
+        pass
 
 
 def main():
@@ -18,7 +21,22 @@ def main():
 
     while exterminator.run:
         exterminator.read_state()
-        exterminator.common_handlers()
+
+        state = exterminator.state
+        actor = state.actors.mine.first
+        base = state.bases.mine.first
+        food = state.foods.closest_to(actor)
+
+        if actor.distance_to(food) < 0.1:
+            actor.take(food)
+        elif actor.food > 100:
+            if actor.distance_to(base) >= 0.1:
+                actor.move(base)
+            else:
+                actor.deposit_food(base)
+        else:
+            actor.move(food)
+
         exterminator.send_commands()
 
 
