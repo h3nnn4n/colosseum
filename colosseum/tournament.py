db8c3b53076d796ba45d9c092687d943723e7165
h3nnn4n <uber.renan@gmail.com>
2021-11-14 10:33:11 -0300

    Implement a proper round robin

--- colosseum/tournament.py
+++ colosseum/tournament.py
@@ -9,21 +9,85 @@ from colosseum.games.food_catcher.game import World
 from .match import match
 
 
-def tournament(agents):
-    results = defaultdict(list)
+INITIAL_ELO = 1200
 
-    for agent_bracket in itertools.combinations(agents, 2):
-        print(f"{Path(agent_bracket[0]).stem} vs {Path(agent_bracket[1]).stem}")
+
+class Game:
+    def __init__(self, *args):
+        self.players = args
+        self.result = None
+
+    @property
+    def n_players(self):
+        return len(self.players)
+
+    @property
+    def rankings(self):
+        return sorted(self.result, key=lambda x: x["score"], reverse=True)
+
+    @property
+    def draw(self):
+        scores = self.rankings.values()
+        return scores[0] == scores[1]
+
+    @property
+    def has_winner(self):
+        scores = self.rankings.values()
+        return scores[0] > scores[1]
+
+    @property
+    def pretty_results(self):
+        return "\n".join(
+            f'{ranking["name"]} {ranking["score"]:.2f}' for ranking in self.rankings
+        )
+
+
+class Participant:
+    def __init__(self, agent_path):
+        self.agent_path = agent_path
+        self.wins = 0
+        self.loses = 0
+        self.draws = 0
+        self.score = 0
+        self.elo = INITIAL_ELO
+
+    def win(self):
+        self.wins += 1
+        self.score += 1
+
+    def lose(self):
+        self.lose += 1
+
+    def draws(self):
+        self.draws += 0
+        self.score += 0.5
+
+    @property
+    def pretty_name(self):
+        return Path(self.agent_path).stem
+
+
+def round_robin(participants, n_rounds=1, n_participants_per_round=2):
+    games = []
+
+    for agent_bracket in itertools.combinations(participants, n_participants_per_round):
+        print(f'participants: {" vs ".join([a.pretty_name for a in agent_bracket])}')
+        game = Game(agent_bracket)
 
         world = World()
-        scores = match(world, agent_bracket)
-        key = "_agents".join(agent_bracket)
-
-        print(
-            f"result: {scores[0]['name']} with {scores[0]['score']:.2f} to {scores[1]['score']:.2f}"
-        )
+        game.result = match(world, [a.agent_path for a in agent_bracket])
+        print(game.pretty_results)
         print()
 
-        results[key].append(scores)
+    return games
 
-    return results
+
+def tournament(agent_paths, mode):
+    participants = [Participant(agent_path) for agent_path in agent_paths]
+
+    if mode == "ROUND_ROBIN":
+        return round_robin(participants)
+    if mode == "DOUBLE_ROUND_ROBIN":
+        return round_robin(participants, n_rounds=2)
+    if mode == "TRIPLE_ROUND_ROBIN":
+        return round_robin(participants, n_rounds=3)
--- tournament.py
+++ tournament.py
@@ -8,7 +8,7 @@ from colosseum.tournament import tournament
 def main():
     agent_paths = sys.argv[1:]
 
-    tournament(agent_paths)
+    tournament(agent_paths, "TRIPLE_ROUND_ROBIN")
 
 
 if __name__ == "__main__":
