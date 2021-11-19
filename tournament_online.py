e88911e960ccdfc1ad5b2ed7532a80befde70b2d
h3nnn4n <uber.renan@gmail.com>
2021-11-19 18:45:19 -0300

    Get agent id from cli args

--- tournament_online.py
+++ tournament_online.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+import sys
+
 from colosseum.tournament_online import online_tournament
 
 
 if __name__ == "__main__":
-    online_tournament("4e829daa-9266-4ecf-9665-2629fb868058")
+    tournament_id = sys.argv[1]
+    online_tournament(tournament_id)
