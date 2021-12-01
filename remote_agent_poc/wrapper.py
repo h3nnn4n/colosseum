3d2cce7a5f589e55de1df3bb5845e5452531e864
h3nnn4n <uber.renan@gmail.com>
2021-11-30 21:30:27 -0300

    Test agent "boot"

--- remote_agent_poc/wrapper.py
+++ remote_agent_poc/wrapper.py
@@ -9,17 +9,18 @@ wrapee_path = "./agent.py"
 def exchange_message(child_process, data_in):
     # print(f"sending {data_in.strip()}")
     child_process.sendline(data_in)
-    data_out = child_process.readline().decode()
-    # print(f"got {data_out.strip()}")
+    data_out = child_process.readline().decode().strip()
+    # print(f"got {data_out}")
     return data_out
 
 
 def main():
     wrapped = PopenSpawn(wrapee_path)
-    print(exchange_message(wrapped, str(2)))
-    print(exchange_message(wrapped, str(3)))
-    print(exchange_message(wrapped, str(4)))
-    print(exchange_message(wrapped, str(5)))
+    assert exchange_message(wrapped, str(2)) == "True"
+    assert exchange_message(wrapped, str(3)) == "True"
+    assert exchange_message(wrapped, str(4)) == "False"
+    assert exchange_message(wrapped, str(5)) == "True"
+    assert exchange_message(wrapped, str(6)) == "False"
 
     # server_address = ("localhost", 10000)
     # s = socket.connect(server_address)
