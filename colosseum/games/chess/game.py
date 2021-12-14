cd99f067130ce3cbf079b5d0eaeba00718148753
h3nnn4n <uber.renan@gmail.com>
2021-12-14 09:05:05 -0300

    Add basic gameplay

--- colosseum/games/chess/game.py
+++ colosseum/games/chess/game.py
@@ -36,12 +36,14 @@ class Game:
 
     @property
     def state(self):
+        legal_moves = [str(m) for m in self._legal_moves]
         return {
             "fen": self._board.fen(),
             "epd": self._board.epd(),
+            "turn": self._board.turn,
             # "outcome": self._board.outcome(),
-            "last_move_san": self._last_move_san,
-            "last_move_lan": self._last_move_lan,
+            "legal_moves": legal_moves,
+            "last_move": self._last_move_uci,
         }
 
     @property
@@ -64,6 +66,12 @@ class Game:
         return self._board.lan(self._last_move)
 
     @property
+    def _last_move_uci(self):
+        if self._last_move is None:
+            return None
+        return self._board.uci(self._last_move)
+
+    @property
     def _legal_moves(self):
         return self._board.legal_moves
 
@@ -73,11 +81,24 @@ class Game:
 
     @property
     def scores(self):
-        return {}
+        data = {}
+
+        # FIXME: We need to get the actual score
+        for agent_id in self.agents:
+            data[agent_id] = 1
+
+        return data
 
     def update(self, agent_actions):
         for agent_action in agent_actions:
             self.process_agent_actions(agent_action)
 
     def process_agent_actions(self, agent_action):
-        pass
+        move_str = agent_action.get("move")
+        if not move_str:
+            return
+
+        move = chess.Move.from_uci(move_str)
+        # FIXME: We should handle this gracefully
+        assert move in self._legal_moves
+        self._board.push(move)
