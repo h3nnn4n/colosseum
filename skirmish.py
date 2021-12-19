a75234bbddfbcb1ecf933bd6a838cd9db39fd77c
h3nnn4n <uber.renan@gmail.com>
2021-12-19 10:25:19 -0300

    Use argparse for handling args

--- skirmish.py
+++ skirmish.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
 import logging
 import sys
@@ -10,12 +11,14 @@ from colosseum.match import match
 from colosseum.utils import get_internal_id
 
 
-game_name = "chess"
+parser = argparse.ArgumentParser(
+    description="Play a one-off match between local agents, without persisting any results."
+)
+parser.add_argument("--game", required=True, type=str)
+parser.add_argument("--agent", action="append", required=True, type=str)
 
 
-def main():
-    agent_paths = sys.argv[1:]
-
+def main(game_name, agent_paths):
     logging.basicConfig(filename=f"match_{get_internal_id()}.log", level=logging.INFO)
 
     if game_name == "chess":
@@ -29,4 +32,5 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    config = vars(parser.parse_args())
+    main(game_name=config["game"], agent_paths=config["agent"])
