583d7647d277362782cdee005ee7f6f86399b5ae
h3nnn4n <uber.renan@gmail.com>
2021-12-26 14:31:11 -0300

    Create automated seasons

--- colosseum/watchdog.py
+++ colosseum/watchdog.py
@@ -15,6 +15,16 @@ API_TOKEN = os.environ.get("API_TOKEN")
 LOOP_INTERVAL = 3
 
 
+def create_automated_seasons():
+    logging.info("Creating automated seasons")
+    response = requests.post(
+        API_URL + "automated_seasons/", headers={"authorization": f"token {API_TOKEN}"}
+    )
+
+    if response.status_code >= 400:
+        logging.warning(f"{response.status_code} {response.content}")
+
+
 def create_automated_tournaments():
     logging.info("Creating automated tournaments")
     response = requests.post(
@@ -50,11 +60,14 @@ def get_agents():
 
 def main():
     while True:
+        logging.info("-" * 64)
         t = time()
-        create_pending_matches()
+        create_automated_seasons()
         create_automated_tournaments()
+        create_pending_matches()
         get_agents()
         t_diff = time() - t
 
         if t_diff < LOOP_INTERVAL:
+            logging.info(f"sleeping {LOOP_INTERVAL - t_diff}")
             sleep(LOOP_INTERVAL - t_diff)
