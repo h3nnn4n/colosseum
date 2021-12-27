72f73781721eeee6bd30d70a4bf711264288c9df
h3nnn4n <uber.renan@gmail.com>
2021-12-26 21:25:45 -0300

    Add script to start one worker per cpu core

---
+++ start_workers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+N_CORES=$(grep -c ^processor /proc/cpuinfo)
+N_CORES=${N_CORES:-2}
+
+echo "Starting ${N_CORES} workers"
+
+for i in "${N_CORES}"; do
+  while [[ true ]]; do python -m poetry run python tournament_online.py; done &
+  sleep 1
+done
+
+echo Done
