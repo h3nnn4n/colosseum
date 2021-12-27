337b8f1bd9c5e36aa31768d2a32782d4049f281c
h3nnn4n <uber.renan@gmail.com>
2021-12-26 21:58:39 -0300

    Add flag to run workers in a loop

--- tournament_online.py
+++ tournament_online.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse
 
 from colosseum.tournament_online import online_tournament
 
 
 if __name__ == "__main__":
-    online_tournament()
+    parser = argparse.ArgumentParser(description="Run tournament matches from an API")
+    parser.add_argument("--loop", action="store_true", help="Runs in a loop")
+    args = vars(parser.parse_args())
+
+    if not args.get("loop"):
+        online_tournament()
+    else:
+        while True:
+            try:
+                online_tournament()
+            except Exception:
+                pass
