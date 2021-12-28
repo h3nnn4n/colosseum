359c7ac03c1f13a8d42a0f4f438292e48ffb47b1
h3nnn4n <uber.renan@gmail.com>
2021-12-28 10:49:31 -0300

    Fix only one worker being started

--- start_workers.sh
+++ start_workers.sh
@@ -5,7 +5,7 @@ N_CORES=${N_CORES:-2}
 
 echo "Starting ${N_CORES} workers"
 
-for i in "${N_CORES}"; do
+for i in `seq "${N_CORES}"`; do
   while [[ true ]]; do python -m poetry run python tournament_online.py; done &
   sleep 1
 done
