b2c09f4072b9a56dd2b4ce755f4f07d82784a711
h3nnn4n <uber.renan@gmail.com>
2022-06-30 21:56:32 -0300

    Add agent spawning and basic updates

--- colosseum/games/snake/game.py
+++ colosseum/games/snake/game.py
@@ -18,6 +18,18 @@ class Direction(Enum):
     DOWN = 3
     LEFT = 4
 
+    @classmethod
+    def from_string(cls, string):
+        match string.lower():
+            case "up":
+                return Direction.UP
+            case "right":
+                return Direction.RIGHT
+            case "down":
+                return Direction.DOWN
+            case "left":
+                return Direction.LEFT
+
 
 class Game(BaseGame):
     def __init__(self):
@@ -26,19 +38,21 @@ class Game(BaseGame):
 
         self._config = Config
         self.name = self._config.game_name
-        self.snakes = []
-        self.snakes_by_id = []
 
+        self.snakes = []
+        self.snakes_by_id = {}
         self.foods = []
+
+        self._tick = 0
 
         logging.info("snake initialized")
 
-    def register(self, agent_id):
-        super().register_agent(agent_id)
+    def register_agent(self, agent):
+        super().register_agent(agent)
 
-        snake = Snake(agent_id)
+        snake = self._spawn_snake(agent.id)
         self.snakes.append(snake)
-        self.snakes_by_id[agent_id] = snake
+        self.snakes_by_id[agent.id] = snake
 
     @property
     def state(self):
@@ -60,7 +74,7 @@ class Game(BaseGame):
 
     @property
     def finished(self):
-        return False
+        return self._tick < self._config.n_epochs
 
     def update(self, agent_actions):
         for agent_action in agent_actions:
@@ -68,13 +82,36 @@ class Game(BaseGame):
 
         self._update_foods()
 
-    def _process_agent_actions(self, agent_action):
-        pass
+    def _process_agent_action(self, agent_action):
+        owner_id = agent_action.get("agent_id")
+        if owner_id not in self.agent_ids:
+            logging.warning(f"agent with id {owner_id} is not registered. Ignoring")
+            return
+
+        move_direction = Direction.from_string(agent_action.get("move"))
+        snake = self.snakes_by_id[owner_id]
+        snake.update(move_direction)
 
     def _update_foods(self):
         if len(self.foods) < self._config.min_food_sources:
             # Spawn
             pass
+
+    def _spawn_snake(self, agent_id):
+        # TODO: We should make sure we do not generate invalid
+        # starting positions, like OOB, instant game over, overlapping
+        # with itself or other snakes, over obstacles or food.
+        starting_position = Vector()
+        tail_position = starting_position.clone()
+        tail_position.x -= 1
+        snake = Snake(agent_id, position=starting_position, head=True)
+        snake_tail = Snake(agent_id, position=tail_position)
+        snake_tail.size = 1
+
+        snake.next_cell = snake_tail
+        snake.next_cell_direction = Direction.LEFT
+
+        return snake
 
 
 class Snake:
@@ -83,6 +120,7 @@ class Snake:
         self.size = 2
         # FIXME: Should be based on spawn position
         self.next_cell_direction = Direction.DOWN
+        self.next_cell = None
         self.is_head = head
         self.position = position
 
@@ -108,10 +146,13 @@ class Snake:
                 self.next_cell_direction = Direction.RIGHT
 
         if self.size > 1:
-            self.next_cell._update()
+            self.next_cell._update(direction)
 
 
 class Vector:
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
+
+    def clone(self):
+        return Vector(self.x, self.y)
