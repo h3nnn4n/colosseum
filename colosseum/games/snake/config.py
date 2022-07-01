10fcd2bcc43b48f6f0f46fd1b99d07df2a60d5fa
h3nnn4n <uber.renan@gmail.com>
2022-07-01 19:05:34 -0300

    Simpler config for debug

--- colosseum/games/snake/config.py
+++ colosseum/games/snake/config.py
@@ -6,11 +6,11 @@ class Config:
     game_name = "snake"
     update_mode = "SIMULTANEOUS"
 
-    grid_width = 40
-    grid_height = 40
+    grid_width = 10
+    grid_height = 10
     food_sources_to_spawn = 2
     min_food_sources = 1
-    n_epochs = 100
+    n_epochs = 5
 
     # Time settings
     step_time_limit = ONE_SECOND * 2
