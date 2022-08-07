b1371ecc630173fe472f2f1da7bfb49c5e39ec64
h3nnn4n <git@renan.digital>
2022-08-07 12:37:08 -0300

    Make games shorter, for debugging

--- colosseum/games/snake/config.py
+++ colosseum/games/snake/config.py
@@ -10,7 +10,7 @@ class Config:
     grid_height = 10
     food_sources_to_spawn = 2
     min_food_sources = 1
-    n_epochs = 500
+    n_epochs = 25
 
     # Time settings
     step_time_limit = ONE_SECOND * 2
