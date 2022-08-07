d61526bded150d1bc910c90dc0a0a14a9c13d108
h3nnn4n <git@renan.digital>
2022-08-07 13:12:37 -0300

    Change n epochs to 1k

--- colosseum/games/snake/config.py
+++ colosseum/games/snake/config.py
@@ -10,7 +10,7 @@ class Config:
     grid_height = 10
     food_sources_to_spawn = 5
     min_food_sources = 1
-    n_epochs = 25
+    n_epochs = 1000
 
     # Time settings
     step_time_limit = ONE_SECOND * 2
